ac60c36506293b0dff630f61e165209469165fca
Ugne Adamo <120172293+uadamo@users.noreply.github.com>
2024-10-07 22:13:05 +0200

    fix: debugging

--- src/Pages/Task3.tsx
+++ src/Pages/Task3.tsx
@@ -69,8 +69,7 @@ const Task3 = () => {
       end_time: endTime,
       timestamp: timestamp,
     }).catch((error) => alert(error));
-    const userListRef = push(ref(db, `users/${userId}/${key}`));
-    await update(userListRef, {
+    await update(ref(db, `users/${userId}/${key}`), {
       session: Number(user.session) + 1,
       nextSessionTime: timestamp + 3600000 * 24,
     }).catch((error) => alert(error));
